8fda907c089f929b27ad3d6da671e5e601a78b4b
pfistfl <pfistererf@googlemail.com>
2021-11-15 13:40:23 +0100

    full test

--- tests/testthat/test_reproduce.R
+++ tests/testthat/test_reproduce.R
@@ -4,3 +4,10 @@ test_that("eval_point works", {
   eval_rbv2_super_dataframe(rbv2_super[3,])
   eval_rbv2_super_dataframe(rbv2_super[sample(100,1),])
 })
+
+test_that("eval_point works", {
+  skip("Long")
+  for (i in seq_len(100)) {
+    eval_rbv2_super_dataframe(rbv2_super[2,])
+  }
+})
